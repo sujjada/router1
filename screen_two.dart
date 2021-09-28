413a1d47a4c96e8bd5c866edc2da530303032e14
Sujjada jabin <90233176+sujjada@users.noreply.github.com>
2021-09-28 11:38:48 +0600

    Update screen_two.dart

--- screen_two.dart
+++ screen_two.dart
@@ -21,4 +21,55 @@ class ScreenTwo extends StatelessWidget {
       ),
     );
   }
+}//the end
+
+
+
+
+
+
+
+import 'package:demo_project/screen_three.dart';
+import 'package:flutter/material.dart';
+
+class ScreenTwo extends StatelessWidget {
+  const ScreenTwo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(" ScreenTwo"),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.arrow_back_ios)),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return ScreenThree();
+                  }));
+                },
+                child: Text("screen Three")),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Back")),
+          ],
+        ),
+      ),
+    );
+  }
 }
+
