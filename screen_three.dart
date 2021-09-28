31695e253ba989982be2216dc1ad1804e29adc14
Sujjada jabin <90233176+sujjada@users.noreply.github.com>
2021-09-28 11:39:39 +0600

    Update screen_three.dart

--- screen_three.dart
+++ screen_three.dart
@@ -15,3 +15,33 @@ class ScreenThree extends StatelessWidget {
     );
   }
 }
+//the end
+import 'package:flutter/material.dart';
+
+class ScreenThree extends StatelessWidget {
+  const ScreenThree({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(" ScreenThree"),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_ios),
+        ),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+              Navigator.pop(context);
+            },
+            child: Text("back")),
+      ),
+    );
+  }
+}
+
